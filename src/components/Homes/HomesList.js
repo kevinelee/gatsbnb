3693eb8bc2ecf5e161ce52de508b0721e065ee73
Kevin Lee <kevin.ej.lee@gmail.com>
2021-01-15 20:54:17 -0800

    completed

--- src/components/Homes/HomesList.js
+++ src/components/Homes/HomesList.js
@@ -22,7 +22,6 @@ function HomeItem({
   baths,
   amenities,
 }) {
-
   return (
     <div className="flex border-b border-gray-200 p-4 pl-0 flex-col lg:flex-row">
       <img className="lg:w-72 lg:h-54 object-cover rounded" src={src} />
@@ -42,63 +41,15 @@ function HomeItem({
   );
 }
 
-const homesArr = [
-  {
-    img:
-      "https://a0.muscache.com/im/pictures/e0a7f934-605a-49cc-8893-f747f7835967.jpg?im_w=1200",
-    location: "Crestline",
-    title: "Get Away and Relax at the Mountains",
-    guests: 6,
-    bedrooms: 3,
-    beds: 4,
-    baths: 2,
-    amenities: "Self check-in · Wifi · Free parking · Pets Allowed",
-  },
-
-  {
-    img:
-      "https://a0.muscache.com/im/pictures/f5174bf4-94e7-4d3e-bdae-f12d83896d75.jpg?im_w=1200",
-    location: "Lake Arrowhead",
-    title: "Lake Arrowhead Tranquil Mountain Retreat",
-    guests: 6,
-    bedrooms: 3,
-    beds: 4,
-    baths: 2,
-    amenities: "Self check-in · Air conditioning · Wifi · Free parking",
-  },
-
-  {
-    img:
-      "https://a0.muscache.com/im/pictures/a8a64b94-c5a7-452a-9c58-a66cfdab8537.jpg?im_w=1200",
-    location: "Lake Arrowhead",
-    title: "Romantic Moutain cabin with full AC & EV Charging",
-    guests: 2,
-    bedrooms: 1,
-    beds: 1,
-    baths: 1.5,
-    amenities: "Self check-in · Air conditioning · Free parking",
-  },
-
-  {
-    img:
-      "https://a0.muscache.com/im/pictures/80d7dd43-01c3-491a-aa4a-2f68d8e8f3f1.jpg?im_w=1200",
-    location: "Lake Arrowhead",
-    title: "Roots Moutain House ~Modern A-Frame~",
-    guests: 9,
-    bedrooms: 3,
-    beds: 6,
-    baths: 2,
-    amenities: "Self check-in · Air conditioning · Free parking · Wifi",
-  },
-];
-
 export default function HomesList({ homes }) {
-  const x = homes?.[0] ? homes[0].location : null;
-  console.log(x);
+  // const x = homes?.[0] ? homes[0].location : null;
+  // console.log("homesList", x);
+
+  console.log("yayeet", homes);
 
   return (
     <section className="w-screen lg:w-1/2 pl-4">
-      <div className="border-b border-gray-200 pb-4">
+      <div className="border-b border-gray-200 py-4">
         <p>300+ stays</p>
         <h1 className="font-bold text-3xl">Stays in Big Bear Lake</h1>
         <div className="my-5">
@@ -115,23 +66,22 @@ export default function HomesList({ homes }) {
       </div>
 
       <div>
-        {homesArr
-          ? homesArr.map((item) => {
+        {homes
+          ? homes.map((item) => {
               const {
-                img,
-                location,
-                title,
-                guests,
+                amenities,
+                baths,
                 bedrooms,
                 beds,
-                baths,
-                amenities,
+                guests,
+                image,
+                key,
+                title,
               } = item;
               return (
                 <HomeItem
-                  key={img}
-                  src={img}
-                  location={location}
+                  key={key}
+                  src={image}
                   title={title}
                   guests={guests}
                   bedrooms={bedrooms}
@@ -146,3 +96,53 @@ export default function HomesList({ homes }) {
     </section>
   );
 }
+
+// const homesArr = [
+//   {
+//     img:
+//       "https://a0.muscache.com/im/pictures/e0a7f934-605a-49cc-8893-f747f7835967.jpg?im_w=1200",
+//     location: "Crestline",
+//     title: "Get Away and Relax at the Mountains",
+//     guests: 6,
+//     bedrooms: 3,
+//     beds: 4,
+//     baths: 2,
+//     amenities: "Self check-in · Wifi · Free parking · Pets Allowed",
+//   },
+
+//   {
+//     img:
+//       "https://a0.muscache.com/im/pictures/f5174bf4-94e7-4d3e-bdae-f12d83896d75.jpg?im_w=1200",
+//     location: "Lake Arrowhead",
+//     title: "Lake Arrowhead Tranquil Mountain Retreat",
+//     guests: 6,
+//     bedrooms: 3,
+//     beds: 4,
+//     baths: 2,
+//     amenities: "Self check-in · Air conditioning · Wifi · Free parking",
+//   },
+
+//   {
+//     img:
+//       "https://a0.muscache.com/im/pictures/a8a64b94-c5a7-452a-9c58-a66cfdab8537.jpg?im_w=1200",
+//     location: "Lake Arrowhead",
+//     title: "Romantic Moutain cabin with full AC & EV Charging",
+//     guests: 2,
+//     bedrooms: 1,
+//     beds: 1,
+//     baths: 1.5,
+//     amenities: "Self check-in · Air conditioning · Free parking",
+//   },
+
+//   {
+//     img:
+//       "https://a0.muscache.com/im/pictures/80d7dd43-01c3-491a-aa4a-2f68d8e8f3f1.jpg?im_w=1200",
+//     location: "Lake Arrowhead",
+//     title: "Roots Mountain House ~Modern A-Frame~",
+//     guests: 9,
+//     bedrooms: 3,
+//     beds: 6,
+//     baths: 2,
+//     amenities: "Self check-in · Air conditioning · Free parking · Wifi",
+//   },
+// ];
